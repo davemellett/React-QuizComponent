7a9345b55f9899fbbbad3046d889b99839743246
Dave Mellett <dave@nicelittlewebsite.co.uk>
2020-05-13 15:49:07 +0100

    Module 3

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -6,7 +6,9 @@ class QuizQuestion extends Component {
 
         return <main>
         <section>
-            {this.props.quiz_question.instruction_text}
+            <p>
+                {this.props.quiz_question.instruction_text}
+            </p>
         </section>
         <section className="buttons">
             <ul>
